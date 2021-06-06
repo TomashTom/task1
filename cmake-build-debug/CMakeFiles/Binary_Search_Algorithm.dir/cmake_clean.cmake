21710c6ed7630db29d20a62cbc8f7ab06e791137
TomashTom <smolskijt@gmail.com>
2021-06-06 20:12:52 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/Binary_Search_Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Binary_Search_Algorithm.exe"
+  "Binary_Search_Algorithm.exe.manifest"
+  "Binary_Search_Algorithm.pdb"
+  "CMakeFiles/Binary_Search_Algorithm.dir/main.cpp.obj"
+  "libBinary_Search_Algorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Binary_Search_Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
